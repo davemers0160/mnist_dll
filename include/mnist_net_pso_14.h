d38d3055fcf8ae09b607ed3799a48346aa138f26
davemers <davemers@iu.edu>
2019-08-15 23:32:58 -0400

    update to new net definition header

--- include/mnist_net_pso_14.h
+++ include/mnist_net_pso_14.h
@@ -11,9 +11,6 @@
 #include "dlib/dnn.h"
 #include "dlib/dnn/core.h"
 
-#include "dlib_elu.h"
-#include "dlib_srelu.h"
-
 extern const std::string version = "pso_14_135";
 //-----------------------------------------------------------------
 
