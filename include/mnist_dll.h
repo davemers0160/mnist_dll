e2aefdc1832c80f8f295e5904030d426dd9463a7
davemers0160 <davemers@umail.iu.edu>
2019-08-19 15:52:46 -0400

    converting to matlab readable dll

--- include/mnist_dll.h
+++ include/mnist_dll.h
@@ -1,7 +1,10 @@
 #ifndef MNIST_DLL_H
 #define MNIST_DLL_H
 
-
+//#define EXTERN_C
+//#include <cstdint>
+//#include <string>
+//#include <vector>
 
 #ifdef MNIST_DLL_EXPORTS
 #define MNIST_DLL_API __declspec(dllexport)
@@ -9,37 +12,87 @@
 #define MNIST_DLL_API __declspec(dllimport)
 #endif
 
-
 struct layer_struct
 {
-    uint64_t k;
-    uint64_t n;
-    uint64_t nr;
-    uint64_t nc;
-    uint64_t size;
-    const float *params;
+    unsigned int k;
+    unsigned int n;
+    unsigned int nr;
+    unsigned int nc;
+    unsigned int size;
 };
 
-
-
+#ifdef __cplusplus
+extern "C" {
+#endif
 // This function will initialize the network and load the required weights
-extern "C" MNIST_DLL_API void init_net(const char *net_name);
+MNIST_DLL_API void init_net(const char *net_name);
+#ifdef __cplusplus
+}
+#endif
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 // This function will take an grayscale image in std::vector<uint8_t> row major order
 // as an input and produce a resulting classification of the image.  The input must be 28*28
-extern "C" MNIST_DLL_API uint64_t run_net(uint8_t input[], uint32_t nr, uint32_t nc);
+MNIST_DLL_API unsigned int run_net(unsigned char input[], unsigned int nr, unsigned int nc);
+#ifdef __cplusplus
+}
+#endif
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 // This function will output a vector of the output layer for the final classification layer
-extern "C" MNIST_DLL_API void get_layer_01(struct layer_struct &data, const float* &data_params);
+//MNIST_DLL_API void get_layer_01(struct layer_struct &data, const float* &data_params);
+MNIST_DLL_API void get_layer_01(struct layer_struct *data, const float* data_params[]);
+#ifdef __cplusplus
+}
+#endif
 
-extern "C" MNIST_DLL_API void get_layer_02(struct layer_struct &data, const float* &data_params);
+#ifdef __cplusplus
+extern "C" {
+#endif
+    //MNIST_DLL_API void get_layer_02(struct layer_struct &data, const float* &data_params);
+    MNIST_DLL_API void get_layer_02(struct layer_struct *data, const float* data_params[]);
+#ifdef __cplusplus
+}
+#endif
 
-extern "C" MNIST_DLL_API void get_layer_05(struct layer_struct &data, const float* &data_params);
+#ifdef __cplusplus
+extern "C" {
+#endif
+    //MNIST_DLL_API void get_layer_05(struct layer_struct &data, const float* &data_params);
+    MNIST_DLL_API void get_layer_05(struct layer_struct *data, const float* data_params[]);
+#ifdef __cplusplus
+}
+#endif
 
-extern "C" MNIST_DLL_API void get_layer_08(struct layer_struct &data, const float* &data_params);
+#ifdef __cplusplus
+extern "C" {
+#endif
+    //MNIST_DLL_API void get_layer_08(struct layer_struct &data, const float* &data_params);
+    MNIST_DLL_API void get_layer_08(struct layer_struct *data, const float* data_params[]);
+#ifdef __cplusplus
+}
+#endif
 
-extern "C" MNIST_DLL_API void get_layer_09(struct layer_struct &data, const float* &data_params);
+#ifdef __cplusplus
+extern "C" {
+#endif
+    //MNIST_DLL_API void get_layer_09(struct layer_struct &data, const float* &data_params);
+    MNIST_DLL_API void get_layer_09(struct layer_struct *data, const float* data_params[]);
+#ifdef __cplusplus
+}
+#endif
 
-extern "C" MNIST_DLL_API void get_layer_12(struct layer_struct &data, const float* &data_params);
+#ifdef __cplusplus
+extern "C" {
+#endif
+    //MNIST_DLL_API void get_layer_12(struct layer_struct &data, const float* &data_params);
+    MNIST_DLL_API void get_layer_12(struct layer_struct *data, const float* data_params[]);
+#ifdef __cplusplus
+}
+#endif
 
 #endif  // MNIST_DLL_H
--- src/mnist_dll_v1.cpp
+++ src/mnist_dll_v1.cpp
@@ -31,82 +31,82 @@ void init_net(const char *net_name)
 
 //----------------------------------------------------------------------------------
 //uint64_t run_net(std::vector<uint8_t> &input)
-uint64_t run_net(uint8_t input[], uint32_t nr, uint32_t nc)
+unsigned int run_net(unsigned char input[], unsigned int nr, unsigned int nc)
 {
     dlib::matrix<uint8_t> ti = dlib::pointer_to_matrix(input, nr, nc); //dlib::reshape(dlib::mat(std::vector<uint8_t>(input, input+length), 28, 28);
     return net(ti);
 }
 
 //----------------------------------------------------------------------------------
-void get_layer_01(struct layer_struct &data, const float* &data_params)
+void get_layer_01(struct layer_struct *data, const float* data_params[])
 {
     auto& lo = dlib::layer<1>(net).get_output();
-    data.k = lo.k();
-    data.n = lo.num_samples();
-    data.nr = lo.nr();
-    data.nc = lo.nc();
-    data.size = lo.size();
-    data_params = lo.host();
+    data->k = lo.k();
+    data->n = lo.num_samples();
+    data->nr = lo.nr();
+    data->nc = lo.nc();
+    data->size = lo.size();
+    *data_params = lo.host();
 }
 
 //----------------------------------------------------------------------------------
-void get_layer_02(struct layer_struct &data, const float* &data_params)
+void get_layer_02(struct layer_struct *data, const float* data_params[])
 {
     auto& lo = dlib::layer<2>(net).get_output();
-    data.k = lo.k();
-    data.n = lo.num_samples();
-    data.nr = lo.nr();
-    data.nc = lo.nc();
-    data.size = lo.size();
-    data_params = lo.host();
+    data->k = lo.k();
+    data->n = lo.num_samples();
+    data->nr = lo.nr();
+    data->nc = lo.nc();
+    data->size = lo.size();
+    *data_params = lo.host();
 }
 
 //----------------------------------------------------------------------------------
-void get_layer_05(struct layer_struct &data, const float* &data_params)
+void get_layer_05(struct layer_struct *data, const float* data_params[])
 {
     auto& lo = dlib::layer<5>(net).get_output();
-    data.k = lo.k();
-    data.n = lo.num_samples();
-    data.nr = lo.nr();
-    data.nc = lo.nc();
-    data.size = lo.size();
-    data_params = lo.host();
+    data->k = lo.k();
+    data->n = lo.num_samples();
+    data->nr = lo.nr();
+    data->nc = lo.nc();
+    data->size = lo.size();
+    *data_params = lo.host();
 }
 
 //----------------------------------------------------------------------------------
-void get_layer_08(struct layer_struct &data, const float* &data_params)
+void get_layer_08(struct layer_struct *data, const float* data_params[])
 {
     auto& lo = dlib::layer<8>(net).get_output();
-    data.k = lo.k();
-    data.n = lo.num_samples();
-    data.nr = lo.nr();
-    data.nc = lo.nc();
-    data.size = lo.size();
-    data_params = lo.host();
+    data->k = lo.k();
+    data->n = lo.num_samples();
+    data->nr = lo.nr();
+    data->nc = lo.nc();
+    data->size = lo.size();
+    *data_params = lo.host();
 }
 
 //----------------------------------------------------------------------------------
-void get_layer_09(struct layer_struct &data, const float* &data_params)
+void get_layer_09(struct layer_struct *data, const float* data_params[])
 {
     auto& lo = dlib::layer<9>(net).get_output();
-    data.k = lo.k();
-    data.n = lo.num_samples();
-    data.nr = lo.nr();
-    data.nc = lo.nc();
-    data.size = lo.size();
-    data_params = lo.host();
+    data->k = lo.k();
+    data->n = lo.num_samples();
+    data->nr = lo.nr();
+    data->nc = lo.nc();
+    data->size = lo.size();
+    *data_params = lo.host();
 }
 
 //----------------------------------------------------------------------------------
 //void get_layer12(struct layer_struct &data, )
-void get_layer_12(struct layer_struct &data, const float* &data_params)
+void get_layer_12(struct layer_struct *data, const float* data_params[])
 {
     auto& lo = dlib::layer<12>(net).get_output();
-    data.k = lo.k();
-    data.n = lo.num_samples();
-    data.nr = lo.nr();
-    data.nc = lo.nc();
-    data.size = lo.size();
-    data_params = lo.host();
+    data->k = lo.k();
+    data->n = lo.num_samples();
+    data->nr = lo.nr();
+    data->nc = lo.nc();
+    data->size = lo.size();
+    *data_params = lo.host();
 }
 
